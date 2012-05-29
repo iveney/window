627a43775ef2d7f394f4cc5f15e70a71953c96e3
Zigang Xiao <iveney@gmail.com>
2012-05-29 15:05:07 -0700

    Update MyBuilder

--- MyBuilder.cpp
+++ MyBuilder.cpp
@@ -1,10 +1,17 @@
+#include <iostream>
+#include <oasis/names.h>
 #include "MyBuilder.h"
+#include "MyCell.h"
+using namespace std;
 
-void MyBuilder::beginFile() {
-  int a = 1;
-}
+//void MyBuilder::beginFile(const string&       version,
+                          //const Oreal&        unit,
+                          //Validation::Scheme  valScheme) {
+  //cout << "Begin File" << endl;
+//}
 
-
-void MyBuilder::endFile() {
-  int a = 1;
+void MyBuilder::beginCell(CellName* cellName) {
+  MyCell* cell = new MyCell(cellName);
+  cellList.push_back(cell);         // create a new cell and store it
+  cellDict.lookup(cellName, true);  // insert into the map
 }
--- MyBuilder.h
+++ MyBuilder.h
@@ -1,20 +1,30 @@
 #ifndef __MYBUILDER_H__
-
 #define __MYBUILDER_H__
 
+#include <iostream>
 #include <oasis/parser.h>
 #include <oasis/builder.h>
+#include "global.h"
 using namespace Oasis;
+using std::string;
 
 class MyBuilder : public OasisBuilder{
 public:
- // override all vertiaul methods
- virtual void beginFile();
- virtual void endFile();
- static void ShowWarning(const char* msg);
+ //MyBuilder() : OasisBuilder() {};
+ 
+ // override virtual methods
+ //virtual void beginFile(const string&       version,
+                        //const Oreal&        unit,
+                        //Validation::Scheme  valScheme);
+ //virtual void registerCellName(CellName* cellName);
+ virtual void beginCell(CellName* cellName);
 
 private:
-  /* data */
+ // needs a vector to store all the cell objects
+ CellList cellList;
+
+ // needs a lookup table (CellDict) to map CellName to Cell Object
+ CellDict cellDict;
 };
 
 #endif /* end of include guard: __MYBUILDER_H__ */
